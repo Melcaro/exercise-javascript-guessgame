e2fb537ddf00192074f1690bcc4e648517539eb6
melanie caro <melanie.caro@stackerine.com>
2019-10-23 20:08:04 +0200

    add advanced version into guessGame.js

--- guessGame.js
+++ guessGame.js
@@ -9,3 +9,33 @@ if (userNumber < mysteryNumber) {
 } else {
   alert('Bravo, vous avez trouvé le chiffre mystère! ');
 }
+
+/*GUESS GAME ADVANCED
+
+const mysteryNumber = 9;
+
+function askUser(numberRepeat) {
+  const userInput = prompt('Devinez le chiffre mystère :');
+  const userNumber = Number(userInput);
+  const hasWon = verifyNumber(userNumber);
+  if (!hasWon && numberRepeat > 1) {
+    askUser(numberRepeat - 1);
+  } else if (!hasWon && numberRepeat === 1) {
+    alert('Vous avez perdu :(');
+  }
+}
+
+function verifyNumber(num) {
+  if (num > mysteryNumber) {
+    alert('Votre chiffre est plus grand que le mien');
+    return false;
+  } else if (num < mysteryNumber) {
+    alert('Votre chiffre est plus petit que le mien');
+    return false;
+  } else {
+    alert('Bravo vous avez trouvé le chiffre mystère !');
+    return true;
+  }
+}
+askUser(5);
+*/
